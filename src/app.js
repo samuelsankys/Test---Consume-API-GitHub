c7b60363424528585cbbd0682e4b7cf2a0169bfc
samuelsankys <ssankys3@gmail.com>
2022-08-13 23:05:13 -0300

    server start

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const cors = require('cors');
+
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS');
+    next();
+});
+
+ 
+module.exports = app;
